4d96c8f63245d66a6fee83f303f634d065c64c71
NachoNav <jose.nfelix@alumnos.upm.es>
2019-03-23 04:43:47 +0100

    Changed control flow and some @staticmethod decorators
    Now the game is run from the file run.py, and the user will be asked to continue playing every round. The I/O functions are managed in a new different file (ask_user.py). I fixed some methods not working properly and some bugs.

--- main.py
+++ main.py
@@ -2,39 +2,29 @@
 # Version: 4.1.0
 # Date:    08/02/17
 import random
-import time
-from datetime import date
 import ascii_dice
+import ask_user as au
 
-# ----------------Display build version and local time---------------------
-today = date.today()
-build_version = 4.1.0
-current_time = time.asctime(time.localtime(time.time()))
-print("\n\nBuild Version:", str(build_version), "\nTime:", str(current_time), "\n\n")
-# -------------------------------------------------------------------------
 
 class Game:
-    # Global Variables
-    bank_roll = 0
-    current_wager = 0
-    lose = False
-    point = 0
-    odds_amount = 0
+    # Attributes
+    __bank_roll = 1000
+    __current_wager = 0
+    __lost = False
+    __point = 0
+    __odds_amount = 0
+    __keep_playing = True
 
-    def __init__(self):
+    def __init__(self, __bank_roll=1000):  # We may add an option to choose initial money later
         self.Game = Game
+        self.__bank_roll = __bank_roll
 
-    @staticmethod
-    def start():
-        intro = True
-        phase1 = True
-        while intro:
-            game.welcome()
-            game.rules()
-            intro = False
-        while phase1:
-            game.come_out_roll()
-        input('done')
+    def start(self):
+        self.welcome()
+        self.rules()
+        while(self.__keep_playing):
+            self.come_out_roll()
+            self.keep_playing()
 
     @staticmethod
     def welcome():
@@ -43,170 +33,135 @@ class Game:
     @staticmethod
     def rules():
 
-        if not Game.ask_yes_no("Would you like to see the rules?"):
+        if not au.ask_yes_no("Would you like to see the rules?"):
             input("Press any key to continue.")
         else:
-            print("\n1. Place a bet.\r\n2. Roll the dice.\r\n3. Get a 7 or 11, you win, get a 2, 3, or 12, you lose, "
-                  "game over.\r\n4. Any other number, that's your point.\r\n5. Keep rolling until...\r\n6. ...you "
-                  "repeat your point, and you win, game over.\r\n7. ...you get a 7, and you lose, game over.\n")
+            print("\n1. Place a bet.\r\n2. Roll the dice.\r\n3. Get a 7 or 11, you win, get a 2, 3, or 12, you __lost, "
+                  "game over.\r\n4. Any other number, that's your __point.\r\n5. Keep rolling until...\r\n6. ...you "
+                  "repeat your __point, and you win, game over.\r\n7. ...you get a 7, and you __lost, game over.\n")
 
-        Game.bank_roll = 1000
         print('You have been assigned an initial bankroll of $1,000, good luck!')
 
-    @staticmethod
-    def come_out_roll():
+    def come_out_roll(self):
+        self.check_broke()
         print("How Much Would you Like to Wager on the Pass Line?")
-        print("Current Bankroll: $", Game.bank_roll)
+        print("Current Bankroll: ${}".format(self.__bank_roll))
 
         # Ask for user input
-        Game.current_wager = Game.ask_for_value("Wager: $")
+        bet = au.ask_for_value("Wager: $")
+        while (bet > self.__bank_roll):
+            bet = au.ask_for_value(
+                "You don't have enough money for that! Please, enter a valid amount: ")
+
+        self.__current_wager = bet
 
         input("Press any key to roll the dice.")
-        Game.point = Game.dice()
+        self.__point = ascii_dice.dice()
 
-        print('The roll is:', Game.point)
+        print('The roll is:', self.__point)
 
-        if Game.point == 2 or Game.point == 3 or Game.point == 12:
-            print("Craps,", Game.point, "Craps... Line Away.")
-            Game.bank_roll = Game.bank_roll - Game.current_wager
-            print("You Lose your bet of $", Game.current_wager, "and now have a total of $", Game.bank_roll)
+        if self.__point == 2 or self.__point == 3 or self.__point == 12:
+            print("Craps,", self.__point, "Craps... Line Away.")
+            self.__bank_roll = self.__bank_roll - self.__current_wager
+            print("You lost your bet of $", self.__current_wager,
+                  "and now have a total of $", self.__bank_roll)
 
-        elif Game.point == 7 or Game.point == 11:
-            print("Winner,", Game.point, "!! Pay the Line!")
-            Game.bank_roll = Game.bank_roll + Game.current_wager
-            print("You Win your bet of $", Game.current_wager, "and now have a total of $", Game.bank_roll)
+        elif self.__point == 7 or self.__point == 11:
+            print("Winner,", self.__point, "!! Pay the Line!")
+            self.__bank_roll = self.__bank_roll + self.__current_wager
+            print("You Win your bet of $", self.__current_wager,
+                  "and now have a total of $", self.__bank_roll)
 
         else:
-            Game.second_phase()
+            self.second_phase()
 
-    @staticmethod
-    def second_phase():
-        print("The point is:", Game.point)
-        print("Would you like to Take odds on your", Game.point, "?")
+    def second_phase(self):
+        print("The __point is:", self.__point)
+        print("Would you like to Take odds on your", self.__point, "?")
 
-        take_odds = Game.ask_yes_no("Take odds? (yes or no) : ")
-        if 'y' or 'yes' not in take_odds:
-            Game.odds_amount = 0
+        take_odds = au.ask_yes_no("Take odds? (yes or no) : ")
+        if not take_odds:
+            self.__odds_amount = 0
             input("Press any key to roll the dice.")
-            Game.streaming_roll()
+            self.streaming_roll()
         else:
-            print("How much you would like would you like to bet behind your", Game.point, "? (Up to 5x): ")
-            odds_amount = Game.ask_for_value("Odds Bet: $")
+            print("How much you would like would you like to bet behind your",
+                  self.__point, "? (Up to 5x): ")
+            __odds_amount = au.ask_for_value("Odds Bet: $")
 
-            if odds_amount > Game.current_wager * 5:
-                print("The Maximum amount of odds you can place behind the", Game.point, "is $", game.current_wager * 5)
+            if __odds_amount > self.__current_wager * 5:
+                print("The Maximum amount of odds you can place behind the",
+                      self.__point, "is $", self.__current_wager * 5)
                 input("Press any key to try again.")
-                Game.second_phase()
+                self.second_phase()
             else:
-                Game.odds_amount = odds_amount
-                Game.streaming_roll()
+                self.__odds_amount = __odds_amount
+                self.streaming_roll()
 
+        #self.keep_playing()
+
+    
                 # TODO: put something here to not let them be able to bet more than they have in the bank
 
-    @staticmethod
-    def streaming_roll():
-        stream = Game.dice()
+    def streaming_roll(self):
+        stream = ascii_dice.dice()
 
-        if stream == Game.point:
-            print("Winner! You Rolled the point of", Game.point)
-            print("You win your initial bet of", Game.current_wager, "plus your odds bet of $", Game.odds_amount)
-            Game.odds(Game.point)
-            print("Total Win: $", Game.current_wager, "(initial wager pays 1:1) and $", Game.odds_amount,
-                  "from your odds on the", Game.point)
-            Game.bank_roll += Game.current_wager
-            Game.bank_roll += Game.odds_amount
-            print("Total Bankroll: $", Game.bank_roll)
+        if stream == self.__point:
+            print("Winner! You Rolled the __point of", self.__point)
+            print("You win your initial bet of", self.__current_wager,
+                  "plus your odds bet of $", self.__odds_amount)
+            self.odds(self.__point)
+            print("Total Win: $", self.__current_wager, "(initial wager pays 1:1) and $", self.__odds_amount,
+                  "from your odds on the", self.__point)
+            self.__bank_roll += self.__current_wager
+            self.__bank_roll += self.__odds_amount
+            print("Total Bankroll: $", self.__bank_roll)
             input("Press Any Key to Continue.")
         elif stream == 7:
             print("Oh dear, big red. Seven OUT, line away.")
-            print("You lose your initial bet of", Game.current_wager, "plus your odds bet of $", Game.odds_amount)
-            print("Total Loss: $", Game.current_wager, "(initial wager) and $", Game.odds_amount, "from your odds on the",
-                  Game.point)
-            Game.bank_roll -= Game.current_wager
-            Game.bank_roll -= Game.odds_amount
-            print("Total Bankroll: $", Game.bank_roll)
+            print("You lost your initial bet of", self.__current_wager,
+                  "plus your odds bet of $", self.__odds_amount)
+            print("Total Loss: $", self.__current_wager, "(initial wager) and $", self.__odds_amount, "from your odds on the",
+                  self.__point)
+            self.__bank_roll -= self.__current_wager
+            self.__bank_roll -= self.__odds_amount
+            print("Total Bankroll: $", self.__bank_roll)
             input("Press Any Key to Continue.")
         else:
             input("Press any key to roll again.")
-            Game.streaming_roll()
+            self.streaming_roll()
 
-    @staticmethod
-    def dice():
-        die1 = random.randint(1, 6)
-        die2 = random.randint(1, 6)
-        dice = die1 + die2
-        print("You roll a", die1, "and", die2, "for a total of", dice)
-        print(ascii_dice.dice[die1 - 1])
-        print(ascii_dice.dice[die2 - 1], '\n')
+    def check_broke(self):
+        if self.__bank_roll <= 0:
+            __lost = True
+            self.__lost_game()
 
-        return dice
-
-    @staticmethod
-    def check_broke():
-        if Game.bank_roll <= 0:
-            lose = True
-
-    def lose_game(self):
+    def __lost_game(self):
         print('Oh dear, you are out of money.')
-        Game.bank_roll = 0
-        Game.current_wager = 0
+        self.__bank_roll = 0
+        self.__current_wager = 0
 
-        if Game.ask_yes_no("Would You like to play again?"):
-            intro = False
+        if au.ask_yes_no("Would You like to play again?"):
+            exit(0)
+            game = Game()
+            game.start
         else:
             quit(0)
 
-    def ask_yes_no(question):
-        response = None
-
-        expected_responses = ('no', 'n', 'yes', 'y')
-
-        while response not in expected_responses:
-            response = input(question).lower()
-
-            # Error message
-            if response not in expected_responses:
-                print('Wrong input please <yes/no>')
-
-        if response == 'yes' or response == 'y':
-            return True
-        else:
-            return False
-
-    def ask_for_value(question):
-        """The solution for asking for a integer value without using exceptions """
-
-        answer = ""
-
-        while not answer.isdigit():
-            answer = input(question)
-
-            # Error message
-            if not answer.isdigit():
-                print("Input must be a number!")
-            else:
-                # Cast for answer
-                return int(answer)
-
-        return answer
-
-    def odds(calculation):
+    def odds(self, calculation):
         if calculation == 4 or calculation == 10:
-            Game.odds_amount *= 2 / 1
+            self.__odds_amount *= 2 / 1
         elif calculation == 5 or calculation == 9:
-            Game.odds_amount *= 3 / 2
+            self.__odds_amount *= 3 / 2
         elif calculation == 6 or calculation == 8:
-            Game.odds_amount *= 6 / 5
+            self.__odds_amount *= 6 / 5
         else:
             pass
-        Game.odds_amount = int(round(Game.odds_amount))
+        self.__odds_amount = int(round(self.__odds_amount))
 
-if __name__ == '__main__':
+    def keep_playing(self):
+        self.__keep_playing = au.ask_to_keep_playing()
+        if not self.__keep_playing:
+            print('Thanks for playing! See ya!')
+            exit(0)
 
-    try:
-        game = Game()
-        game.start()
-    except KeyboardInterrupt:
-        print('\nGamed ended by user!')
-        # TODO: Maybe a little game summary here
-        # This is a good idea, perhaps roll and win/loss history!
---
+++ run.py
@@ -0,0 +1,18 @@
+
+import time
+import main as craps
+
+# ----------------Display build version and local time---------------------
+build_version = '4.1.0'
+current_time = time.asctime(time.localtime(time.time()))
+print("\n\nBuild Version:", build_version,
+      "\nTime:", str(current_time), "\n\n")
+# -------------------------------------------------------------------------
+
+try:
+    game = craps.Game()
+    game.start()
+except KeyboardInterrupt:
+    print('\nGamed ended by user!')
+    # TODO: Maybe a little game summary here
+    # This is a good idea, perhaps roll and win/loss history!
